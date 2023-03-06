70baf6eadd705d8ba0d4970c12b85b3020085f43
Chris Saffron <saffronkrzysztof@gmail.com>
2023-03-06 13:49:44 +0100

    fixed links in preview mode

--- src/pages/StartPage/components/PROJECTLIST.js
+++ src/pages/StartPage/components/PROJECTLIST.js
@@ -3,55 +3,53 @@ const PROJECTLIST = [
         img: 'preview-3dportfolio.jpg',
         name: 'Portfolio - 3D Version',
         description: '',
-        link: 'hhtp:sfdaf',
-        linkGithub: 'http:giththt'
+        link: 'https://portfolio3d-two.vercel.app/',
+        linkGithub: 'https://github.com/Havir-S/portfolio3d/tree/main'
 
     },
     {
         img: 'preview-2dportfolio.jpg',
         name: 'Portfolio - 2D Version',
         description: '',
-        link: 'hhtp:sfdaf',
-        linkGithub: 'http:giththt'
+        link: 'https://portfolio2d.vercel.app/',
+        linkGithub: 'https://github.com/Havir-S/portfolio2d/tree/main'
 
     },
     {
         img: 'preview-blog.jpg',
         name: 'My Blog',
         description: "See what I'm currently working on",
-        link: 'hhtp:sfdaf',
-        linkGithub: 'http:giththt'
+        link: 'https://bloghavir.vercel.app/',
+        linkGithub: 'https://github.com/Havir-S/bloghavir/tree/main'
     },
     {
         img: 'preview-szafranprojekt.jpg',
         name: 'Szafran Projekt',
         description: 'Landing Page for an official business',
-        link: 'hhtp:sfdaf',
-        linkGithub: 'http:giththt'
+        link: 'https://szafranprojekt.vercel.app/',
+        linkGithub: 'https://github.com/Havir-S/szafranprojekt/tree/main'
     },
     {
         img: 'preview-socialmedia1.jpg',
         name: 'Social Media Project',
         description: 'Made by imitating Facebook',
-        link: 'hhtp:sfdaf',
-        linkGithub: 'http:giththt'
+        link: 'https://fb-clone-xi-six.vercel.app/',
+        linkGithub: 'https://github.com/Havir-S/fb-clone/tree/main'
     },
     {
         img: 'preview-socialmedia2.jpg',
         name: 'Social Media Project 2',
         description: 'Made by imitating Instagram',
-        link: 'hhtp:sfdaf',
-        linkGithub: 'http:giththt'
+        link: 'https://instagram-v2-phi.vercel.app/',
+        linkGithub: 'https://github.com/Havir-S/instagram-v2/tree/main'
     },
     {
         img: 'preview-chatapp.jpg',
         name: 'Chat app',
         description: 'Similiar to the Messenger App',
-        link: 'hhtp:sfdaf',
-        linkGithub: 'http:giththt'
+        link: 'https://chatapp-six-pi.vercel.app/',
+        linkGithub: 'https://github.com/Havir-S/chatapp/tree/main'
     },
-    
-    
 ]
 
 export {PROJECTLIST}
